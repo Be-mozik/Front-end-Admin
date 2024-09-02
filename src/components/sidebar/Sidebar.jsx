c00a62627aacc8378cd8ff4843a95e5071a8138f
Rakotoarivo Loick <120171050+Avocadorable0@users.noreply.github.com>
2024-09-02 16:33:22 +0300

    Side nav

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,44 +1,66 @@
 import './Sidebar.css'
 import Logo from '../../assets/Logo.png'
-import React from 'react'
+import React, { useState } from 'react'
 import { TbLayoutDashboardFilled } from "react-icons/tb"
 import { FaUserLock } from "react-icons/fa6"
 import { BsPersonLinesFill } from "react-icons/bs"
 import { IoCalendarSharp } from "react-icons/io5"
-import { Link } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
 
 const Sidebar = () => {
+    const location = useLocation();
+    const [activeLink, setActiveLink] = useState(location.pathname);
+
+    const handleLinkClick = (path) => {
+        setActiveLink(path);
+    };
 
     return(
         <div className="container">
             <div className="header">
                 <div className="logo">
-                    <img src={Logo} alt="" />
+                    <img src={Logo} alt="Logo" />
                 </div>
             </div>
             <div className="trait">
                 <hr />
             </div>
             <div className="liens">
-                <Link to ="/dashboard" className='link-custom'>
+                <Link 
+                    to="/dashboard" 
+                    className={`link-custom ${activeLink === '/dashboard' ? 'active' : ''}`} 
+                    onClick={() => handleLinkClick('/dashboard')}
+                >
                     <div className="submit">
                         <TbLayoutDashboardFilled/>
                         Dashboard
                     </div>
                 </Link>
-                <Link to="/permission" className='link-custom'>
+                <Link 
+                    to="/permission" 
+                    className={`link-custom ${activeLink === '/permission' ? 'active' : ''}`} 
+                    onClick={() => handleLinkClick('/permission')}
+                >
                     <div className="submit">
                         <FaUserLock />
                         Permission
                     </div>
                 </Link>
-                <Link to="/client" className='link-custom'>
+                <Link 
+                    to="/client" 
+                    className={`link-custom ${activeLink === '/client' ? 'active' : ''}`} 
+                    onClick={() => handleLinkClick('/client')}
+                >
                     <div className="submit">
                         <BsPersonLinesFill />    
                         Client
                     </div>
                 </Link>
-                <Link to="/event" className='link-custom'>
+                <Link 
+                    to="/event" 
+                    className={`link-custom ${activeLink === '/event' ? 'active' : ''}`} 
+                    onClick={() => handleLinkClick('/event')}
+                >
                     <div className="submit">
                         <IoCalendarSharp />
                         Événement
